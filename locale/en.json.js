56b759f5fcc9aab9c34da11a66578fd77a00626e
Yuhang <abeyuhang@gmail.com>
2016-10-21 16:18:18 +0800

    update

---
+++ locale/en.json.js
@@ -0,0 +1,7 @@
+(function () {
+const dict = {
+  "app.name": "JINGE DESIGN",
+  "error.unauthorized": "没有权限进行此操作"
+};
+window.__AssetLoader.Asset.locale = dict;
+})();
---
+++ locale/zh_cn.json.js
@@ -0,0 +1,10 @@
+(function () {
+const dict = {
+  "app.name": "静格设计",
+  "email.subject.register": "注册验证码",
+  "email.error.exists": "该邮箱已经被注册",
+  "error.unauthorized": "没有权限进行此操作",
+  "error.need_login": "请先登陆"
+};
+window.__AssetLoader.Asset.locale = dict;
+})();
